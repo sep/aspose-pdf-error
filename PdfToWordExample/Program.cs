e60673dc0516ea352cae1a6cdf470331ab1a1934
Jon Fuller <jcfuller@sep.com>
2020-04-14 07:34:20 -0400

    initial commit

---
+++ PdfToWordExample/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Aspose.Pdf;
+
+namespace PdfToWordExample
+{
+    class Program
+    {
+        private const int ExitCode_Nominal = 0;
+        private const int ExitCode_GeneralError = 1;
+        private const int ExitCode_InvalidArguments = 64;
+
+        static int Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.Error.WriteLine("This program takes 1 argument: the full path to a PDF file to convert to a docx file");
+                return ExitCode_InvalidArguments;
+            }
+            var srcFileName = args.First();
+            var dstFileName = Path.ChangeExtension(srcFileName, "docx");
+
+            const string licenseFile = "Aspose.Total.lic";
+            new License().SetLicense(licenseFile);
+
+            try
+            {
+
+                Document.Convert(srcFileName, null, dstFileName, new DocSaveOptions
+                {
+                    Format = DocSaveOptions.DocFormat.DocX,
+                });
+
+                Console.Out.WriteLine($"converted '{srcFileName}' to '{dstFileName}'");
+                return ExitCode_Nominal;
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.ToString());
+                return ExitCode_GeneralError;
+            }
+        }
+    }
+}
